46ad92b65edf39d035b5182a82ee915eac3dd32f
alwin <alwinsunny123456@gmail.com>
2019-03-24 22:12:35 +1100

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import io from 'socket.io-client';
+
+
+const socket = io('http://localhost:5000/');
 
 class App extends Component {
+
+
+  state = {
+    messages: [],
+    message:''
+  }
+
+  componentDidMount(){
+
+socket.on('ack',(message)=>{
+
+ console.log(message)
+   let messages = this.state.messages;
+   messages.push(message)
+   this.setState({messages:messages})
+  console.log('message', this.state.message)
+})
+
+
+
+
+
+  }
   render() {
+
+    let messages = this.state.messages.map((message)=>{
+      return <div>{message.message}</div>
+    })
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+
+   <input type = 'text' onChange = {(e)=>{
+     this.setState({message:e.target.value})
+     console.log("message",this.state.message)
+   }}/>
+   <br/>
+   <button onClick = {()=>{
+
+      socket.emit('message',{message:this.state.message})
+
+
+   }}>send</button>
+
+    <div>
+
+{messages}
+
+    </div>
+
+
+
       </div>
     );
   }
